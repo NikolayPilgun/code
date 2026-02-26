5b4e853b307eef2d7b89819cf59cdd571e4a7d12
Nikolay Pilgun <a03198814s@gmail.com>
2026-02-26 22:23:39 +0300

    react-router-dom

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,15 @@
-import { BoardPage } from "@/features/board";
+import { AppHeader } from "@/features/header";
+import { ROUTES } from "@/shared/model/routes";
+import { Outlet, useLocation } from "react-router-dom";
 
 export function App() {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
   return (
     <div>
-      <BoardPage />
+      {!isAuthPage && <AppHeader />}
+      <Outlet />
     </div>
   );
 }
